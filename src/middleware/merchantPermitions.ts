ef50887513d66193001e691d230bcc44a0e25488
OLUWASINA OkikiJesu <74295767+okikijesutech@users.noreply.github.com>
2024-03-03 23:26:35 +0100

    code refactoring

--- src/middleware/merchantPermitions.ts
+++ src/middleware/merchantPermitions.ts
@@ -1,1 +1,17 @@
-export const merchantPermission = (req, res, next) => {};
+// export const merchantPermission = (req, res, next) => {};
+// Middleware function to check if the user is logged in as a merchant
+const requireMerchantLogin = async (req, res, next) => {
+  try {
+    // Check if there is a user session and if it's a merchant
+    if (!req.user || !req.user.merchants) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized, merchant login required" });
+    }
+    // If the user is logged in as a merchant, proceed to the next middleware/route handler
+    next();
+  } catch (error) {
+    console.error("Error in requireMerchantLogin middleware:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
