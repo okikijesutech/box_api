2120b892c3c33bb2245a991d3984a67410c9569b
OLUWASINA OkikiJesu <74295767+okikijesutech@users.noreply.github.com>
2024-03-04 21:36:13 +0100

    router update

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import userRouter from "./routes/user.router";
-import adminRouter from "./routes/merchant.router";
+import merchantRouter from "./routes/merchant.router";
 import superAdminRouter from "./routes/admin.router";
 
 const app = express();
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cors(corsOptions));
 
 app.use("/user", userRouter);
-app.use("/merchant", adminRouter);
+app.use("/merchant", merchantRouter);
 app.use("/admin", superAdminRouter);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
